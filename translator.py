c2e9cf918e17924e8fe784888d66340b08420cd8
Jose Carranza <jsl.carranza@gmail.com>
2019-10-16 19:23:56 +0200

    A limpiar

---
+++ translator.py
@@ -0,0 +1,10 @@
+from googletrans import Translator
+
+
+
+def traducer(frase, idi):              
+    traductor=Translator()
+    traduccion=traductor.translate(frase,dest=idi)
+    print(traduccion)
+    return traduccion.text
+
